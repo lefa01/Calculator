b6c69b67e46bbb75630b8f3083609d235b4f5c0d
lefa01 <lefaamani@gmail.com>
2023-10-19 15:48:33 +0400

    Initial commit

---
+++ app/src/main/java/com/hadf/calculatorapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.hadf.calculatorapp
+
+import  android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hadf.calculatorapp.ui.theme.CalculatorAppTheme
+import com.hadf.calculatorapp.ui.theme.MediumGray
+import com.plcoding.calculatorprep.CalculatorViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorAppTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
